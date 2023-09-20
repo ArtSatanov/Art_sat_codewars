3e68c38ee3903581c9fc75f561831a2f10a22aa2
Artem Satanovskiy <artsatanov@gmail.com>
2023-09-20 18:27:06 +0300

    Update js.js

--- js.js
+++ js.js
@@ -12,3 +12,51 @@
 
 // kata 7
 
+// function squareDigits(num) {
+//    const numArray = num.toString().split('');
+//    const newArray = [];
+
+//       numArray.forEach(element => {
+//          newArray.push(Math.pow(Number(element), 2));
+//       });
+   
+//    console.log(newArray.join(''));
+
+//    return Number(newArray.join(''));
+// }
+
+// squareDigits(3212)
+
+
+
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+
+// function getCount(str) {
+//    let result = 0;
+//    const valwes = ["a", "e", "i", "o", "u"];
+//    const strToArray = str.split('');
+//    strToArray.forEach(element => {
+//       if (valwes.find(option => option === element)) {
+//          result++;
+//       }
+//    });
+//    return result;
+// }
+
+
+// kata 5
+
+
+
+
+function two() {
+   
+}
+function three() {}
+
+function plus() {}
