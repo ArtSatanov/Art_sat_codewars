b55ff77fcee0446484c035a1416c53cb70da5c0f
Artem Satanovskiy <artsatanov@gmail.com>
2023-09-20 16:08:05 +0300

    1

---
+++ js.js
@@ -0,0 +1,14 @@
+// kata 8
+// function areYouPlayingBanjo(name) {
+//   const nameSplited = name.split('');
+//   if (nameSplited[0].toLowerCase() === 'r') {
+//     return `${name} plays banjo`
+//   }
+//   // Implement me
+//   return `${name} does not play banjo`;
+// }
+// areYouPlayingBanjo("artme");
+
+
+// kata 7
+
